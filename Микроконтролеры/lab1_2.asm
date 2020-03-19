2c0fc9c7e72ddb5e07708c6eced1b801df31be5e
CyberMakaron <makrusim@yandex.ru>
2020-03-19 22:31:14 +0300

    Update lab1_2.asm

--- Микроконтролеры/lab1_2.asm
+++ Микроконтролеры/lab1_2.asm
@@ -1,18 +1,14 @@
  	;######### System Clock - 12Mhz ########
 org 500h
-	db 23h, 30h, 02ah, 39h, 38h, 37h, 36h, 35h, 34h, 33h, 32h, 31h
+	db 23h, 30h, 02ah, 39h, 38h, 37h, 36h, 35h, 34h, 33h, 32h, 31h ; коды символов соответствующих клавиш
 
 ORG 0
 
 	JMP main
 
-org 13h	; int1
-	setb p0.0
-	setb p0.1
-	setb p0.2
-	setb p0.3	
+org 13h				; прерывание int1	
 	call scanKeyBoard
-	CLR p0.0
+	CLR p0.0		; сброс строк, чтобы можно было обнаружить следующее нажатие
 	CLR p0.1
 	CLR p0.2
 	CLR p0.3
@@ -21,31 +17,31 @@ org 13h	; int1
 org 030h
 main:
 	CLR SM0 
-	SETB SM1 ; 2 режим
+	SETB SM1 	; 2 режим: 8 бит данных + стартовый + стоповый + бит чётности
 
 	MOV A, PCON 
 	SETB ACC.7 
-	MOV PCON, A ; set SMOD in PCON to double baud rate
+	MOV PCON, A 	; set SMOD in PCON to double baud rate (увеличивается точность)
 
-	MOV TMOD, #20H 
-	MOV TH1, #243 
+	MOV TMOD, #20H 	; натсройка таймера: (M1 = 1; M0 = 1) из TH1 записывается в TL1 при переполнении
+	MOV TH1, #243 	; 256 - 12Мгц/12/16/4800
 	MOV TL1, #243 
-	SETB TR1 ; start timer 1
-	SETB IT1 ; по срезу
-	SETB EX1 ; прерывание от INT1
-	SETB EA	; разрешить все прерывания
-	mov dptr, #500h
-	CLR p0.0
+	SETB TR1 	; start timer 1
+	SETB IT1 	; по срезу
+	SETB EX1 	; прерывание от INT1
+	SETB EA		; разрешить все прерывания
+	mov dptr, #500h	; адрес массива символов
+	CLR p0.0	; сброс ножек порта P0, соотв. столбцам, чтобы обнаружить нажатие
 	CLR p0.1
 	CLR p0.2
 	CLR p0.3
 	sjmp $
 
-scanKeyBoard:
-	mov r0, #0
+scanKeyBoard:		; определяет нажатую клавишу (R0 - строка, R1 - столбец)
+	mov r0, #0	; сканируем 0 строку
 	mov p0, #11111110b
- 	CALL scanColumn
- 	JB F0, gotKey
+ 	CALL scanColumn	; сканируем столбцы
+ 	JB F0, gotKey	; если найдена клавиша, то закончить поиск
 
  	inc r0
  	mov p0, #11111101b
@@ -62,12 +58,12 @@ scanKeyBoard:
  	CALL scanColumn
  	JB F0, gotKey
 
-	ret
+	ret	; нажатия не произошло
     gotKey:
-	call sendKey
+	call sendKey	; если клавиша найдена, то передать её по UART
 	ret
 
-scanColumn:
+scanColumn:	; сканирует столбцы на признак нажатия клавиши (F = 1 и R1 - столбец, если нажата, иначе F = 0)
 	mov r1, #0
 	JNB P0.4, gotCol
  	INC R1 
@@ -86,10 +82,10 @@ sendKey:
 	mov b, #3
 	mul ab
 	add a, r1
-	movc a, @a + dptr
-	MOV C, P
-	MOV ACC.7, C ; бит чётности
-	MOV SBUF, A 
-	JNB TI, $ ; ожидание пока установится бит TI
-	CLR TI
+	movc a, @a + dptr	; получаем символ нажатой клавиши
+	MOV C, P	; чётность аккумулятора
+	MOV ACC.7, C 	; бит чётности
+	MOV SBUF, A 	; передаём байт
+	JNB TI, $ 	; ожидание пока установится бит TI - отправка завершена
+	CLR TI		; сброс TI
 	ret
